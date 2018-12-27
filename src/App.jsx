09ba446a72177095ff2ce438f905f184c5a4de3e
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-27 12:25:34 +0100

    configuration, font, assets: webpack 3, babel 7, prettier, eslint, jest

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Blockchain Ico</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At et accusantium nemo provident?</p>
+    </div>
+  )
+}
+
+export default App;
